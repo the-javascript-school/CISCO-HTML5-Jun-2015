7341a14ed92e421b4ee5cda7abcf239af6dd5321
tkmagesh <tkmagesh77@gmail.com>
2015-06-09 11:13:22 +0530

    Worker- Solution

--- 05-WebWorkers/complexWorker.js
+++ 05-WebWorkers/complexWorker.js
@@ -1,11 +1,22 @@
 function doWork(onComplete, onProgress){
-    for(var i=0; i<(10000);i++)
-        for(var j=0; j<10000; j++)
-            for(var k=0; k<100; k++)
-            {}
+    for(var x = 0; x < 100; x++){
+        for(var i=0; i<(10000/100);i++)
+            for(var j=0; j<10000; j++)
+                for(var k=0; k<100; k++)
+                {}
+
+        self.postMessage({
+            type:"progress",
+            percentCompleted : x + 1
+        });
+    }
 }
 //doWork();
 self.addEventListener("message", function(evtArg){
-    console.log("Message from main - ", evtArg.data);
-    self.postMessage("Thanks for the message");
+   if (evtArg.data.type === "start"){
+       doWork();
+       self.postMessage({
+           type : "completion"
+       });
+   }
 })
